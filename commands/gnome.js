8da95980b29c9f0a9a30da5f6fdc4a994b32346a
Xndr <moermanxander@gmail.com>
2022-12-02 21:15:48 +0100

    gnome command

---
+++ commands/gnome.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed, User, Guild, MessageAttachment } = require('discord.js')
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("gnome")
+        .setDescription("Gnome someone")
+        .addUserOption(option =>(
+            option.setName('user')
+                .setDescription('Give the name of someone you want to gnome')
+                .setRequired(true))
+        )
+        .setDMPermission(false),
+            
+    async execute (interaction, client) {
+        if(client.cooldowns.has(interaction.user.id))
+        {
+            interaction.reply({ content: "Please wait for the cooldown to end!", ephemeral: true});
+        }
+        else
+        {
+            const user = interaction.options.getUser('user');
+            const image = new MessageAttachment("https://i.imgflip.com/4u7k6j.jpg");
+            
+            await interaction.reply({ content: `${user}, you have been GNOMED. :joy:`, files: [image] });
+
+
+            // set cooldown
+            client.cooldowns.set(interaction.user.id, true);
+            setTimeout(() => {
+                client.cooldowns.delete(interaction.user.id); //clear cooldown
+            }, client.COOLDOWN_SECONDS * 1000); // after ... seconds
+        }
+
+        //log into file
+        const content = '\ndecide command executed by '+ interaction.member.user.username + ' at ' + new Date().toLocaleString();
+        const path = 'Logs.txt';
+        fs.writeFile(path, content, { flag: 'a' }, err => { //append to file write at end of file
+            if (err) {
+              console.error(err);
+            }
+        });
+    }
+}
